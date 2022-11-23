f0934cb5a241e740192d1afaaab69ec6df911b7a
chahine <chahinelaater91@gmail.com>
2022-11-23 20:28:49 +0100

    third commit

--- src/main/java/com/formation/integcontinue/App.java
+++ src/main/java/com/formation/integcontinue/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Application!" );
+        System.out.println( "Hello World!!!!!!!!!" );
     }
 }
